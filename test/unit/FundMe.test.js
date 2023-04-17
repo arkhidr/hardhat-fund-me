8d80aae35c29f9051d07475284df81d50e9c7c72
arkhidr <arkhidr@gmail.com>
2023-04-17 03:13:07 +0300

    Update NodeModules

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,10 +1,21 @@
+const { assert } = require("chai")
 const { deployments, ethers, getNamedAccounts } = require("hardhat")
+
 describe("FundMe", function () {
-    let fundMe
+    let fundMe, deployer, mockV3Aggregator
     beforeEach(async function () {
-        const { deployer } = await getNamedAccounts()
+        deployer = (await getNamedAccounts()).deployer
         await deployments.fixture(["all"])
-        fundMe = await ethers.getContract("FundMe")
+        fundMe = await ethers.getContract("FundMe", deployer)
+        mockV3Aggregator = await ethers.getContract(
+            "MockV3Aggregator",
+            deployer
+        )
     })
-    describe("constructor", function () {})
+    describe("Constructor", function () {
+        it("1- Sets the aggregator address correctly", async function () {
+            const response = await fundMe.priceFeed()
+            assert.equal(response, mockV3Aggregator.address)
+        })
+    })
 })
